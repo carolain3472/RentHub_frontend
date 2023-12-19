ec5f25641fc6f56a1859e0c7d1b552a55580f6d6
GABO1017 <gabrielfranco1017@gmail.com>
2023-12-19 01:51:13 -0500

    maps everywhere

--- RentHub_frontend/src/components/FormularioObjeto.jsx
+++ RentHub_frontend/src/components/FormularioObjeto.jsx
@@ -196,6 +196,15 @@ export function FormularioObjeto() {
             onChange={fileHandler}
           />
         </div>
+        <div>
+          <label className="form-label">Direccion:</label>
+          <input
+            className="form-input"
+            type="text"
+            id="direccion"
+            placeholder="Direccion de residencia"
+          />
+        </div>
 
         <button className="form-submit-button" type="submit">Guardar</button>
       </form>
